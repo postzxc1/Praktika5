b0c64403a41a0a38183579639c38a166a086f7e4
Mars <m.babiev05@mail.ru>
2022-10-02 22:55:46 +0500

    практика 6

---
+++ Praktika6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Praktika6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aaa = 99;
+            int Flag = 0;
+            double x = 0;
+            Console.WriteLine("x: -4, 4");
+            Console.WriteLine("------------------");
+            for (double y = -4; y < 4; y += 0.5, Flag += 1)
+            {
+                x = y;
+                if (x < 0 & Flag % 2 == 0)
+                {
+                    Console.WriteLine($"x:{x}     y = {Math.Abs(y)}");
+                }
+                else if (x < 0 & Flag % 2 == 1)
+                {
+                    Console.WriteLine($"x:{x}   y = {Math.Abs(y)}");
+                }
+                else if (Flag % 2 == 0)
+                {
+                    Console.WriteLine($"x:{x}      y = {Math.Abs(y)}");
+                }
+                else if (Flag % 2 == 1)
+                {
+                    Console.WriteLine($"x:{x}    y = {Math.Abs(y)}");
+                }
+            }
+        }
+    }
+}
